a8be993334fa6d587742dacc0ac2b5d2857dc120
vadecha shreyash <vadechashreyash07@gmail.com>
2025-11-10 14:05:10 +0530

    Added historyApiFallback for client-side routing

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: "dist",
+  },
+  server: {
+    historyApiFallback: true, // ✅ handle React Router routes in dev mode
+  },
+  preview: {
+    historyApiFallback: true, // ✅ handle routes after build (used by Vercel too)
+  },
+});
