e8de77af20d89c5cd1eaddb7601e2c4d44719182
vadecha shreyash <vadechashreyash07@gmail.com>
2025-11-10 13:19:20 +0530

    Nineth Commit

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar.jsx";
 import Footer from "./Footer.jsx";
-import "../styles/layout.css"
+import "../styles/Layout.css"
 
 function Layout() {
   return (
