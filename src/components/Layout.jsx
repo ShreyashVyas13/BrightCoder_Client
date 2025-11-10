ee19336b2919c71e95cd6758c1c585666df5de0b
vadecha shreyash <vadechashreyash07@gmail.com>
2025-11-10 13:15:46 +0530

    Eigth Commit

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar.jsx";
 import Footer from "./Footer.jsx";
-import "../styles/Layout.css"
+import "../styles/layout.css"
 
 function Layout() {
   return (
